de0b8e6e01890606924776d47d18182f82be7fc2
Anastassia Sommer <anastassia.sommer@gmail.com>
2024-03-05 18:25:58 +0100

    solution

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function (str, number) {
+  let returnStr = '';
+  if (number < 0) return 'ERROR';
+  for (let i = 0; i < number; i++) {
+    returnStr += str;
+  }
+  return returnStr;
 };
 
 // Do not edit below this line
 module.exports = repeatString;
+
+console.log(repeatString('hey', 4));
